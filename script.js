ea900ea472856bcbbe5fefa85410298e888995b4
aob123 <alexobeswick@gmail.com>
2021-12-22 15:01:26 +0100

    second commit

---
+++ script.js
@@ -0,0 +1,10 @@
+let p = document.getElementById("fact");
+let button = document.getElementsByClassName("button")[0];
+
+button.addEventListener("click", () => {
+  fetch("https://catfact.ninja/fact")
+    .then(response => response.json())
+    .then((data) => {
+        p.innerHTML = "<p>" + data.fact + "</p>";
+    });
+});
